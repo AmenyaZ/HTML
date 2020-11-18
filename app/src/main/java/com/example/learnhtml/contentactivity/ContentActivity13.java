761a8f179b609c64df5a25d81acfb000efb00be7
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-11-19 02:10:41 +0300

    Implementing the Webview

--- app/src/main/java/com/example/learnhtml/contentactivity/ContentActivity13.java
+++ app/src/main/java/com/example/learnhtml/contentactivity/ContentActivity13.java
@@ -3,6 +3,7 @@ package com.example.learnhtml.contentactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
