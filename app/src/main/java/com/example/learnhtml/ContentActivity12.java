8df6162088bf775372a28bb520affbc83d5c0b61
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-11-19 00:47:23 +0300

    contentActivity Modified

---
+++ app/src/main/java/com/example/learnhtml/ContentActivity12.java
@@ -0,0 +1,14 @@
+package com.example.learnhtml;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ContentActivity12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_content12);
+    }
+}
