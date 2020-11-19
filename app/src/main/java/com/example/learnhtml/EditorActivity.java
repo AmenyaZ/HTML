14bdc57ca2864709ec835bcc7af41d15777c960b
kamau-mbugua <kamau_mbugua@yahoo.com>
2020-11-19 23:54:40 +0300

    Solved The dimens bug

--- app/src/main/java/com/example/learnhtml/EditorActivity.java
+++ app/src/main/java/com/example/learnhtml/EditorActivity.java
@@ -1,14 +1,26 @@
 package com.example.learnhtml;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EditorActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        ImageView iv_backEditor = findViewById(R.id.iv_backEditor);
+        iv_backEditor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
